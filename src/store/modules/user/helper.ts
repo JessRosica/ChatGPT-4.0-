876ab1047040bf7e73c15d27e314848d2329a672
JessRosica <129259307+JessRosica@users.noreply.github.com>
2023-04-11 20:33:04 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: '蒸汽电饭煲',
+      description: 'Star on <a href="XXX" class="text-blue-500" target="_blank" >QQ售后群</a>',
     },
   }
 }
